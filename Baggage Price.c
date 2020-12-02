518c2c7783fde5115ed1caa04f78c6295974a737
Enes <60428493+enesbilik@users.noreply.github.com>
2020-12-02 20:57:25 +0300

    Update Baggage Price.c

--- Baggage Price.c
+++ Baggage Price.c
@@ -6,4 +6,18 @@ kg başına 5 tl ödeme yapmak durumundadır. Buna göre klavyeden el ve normal 
 kg olarak ağırlığını giren yolcunun ödemesi gereken bagaj ücretini hesaplayan kod ?
 */
 
+#include <stdio.h>
+int main() {
+    int elHak = 8, normalHak = 15;
+    int el, normal, elUcret, normalUcret;
 
+    printf("El ve bagaj agirligini giriniz:");
+    scanf("%d %d", &el, &normal);
+
+    elUcret = (el - elHak) * 4 > 0 ? (el - elHak) * 4 : 0;
+    normalUcret = (normal - normalHak) * 5 > 0 ? (normal - normalHak) * 5 : 0;
+
+    printf("Odemeniz gerken toplam ucret %d TL'dir.", elUcret + normalUcret);
+
+    return 0;
+}
