5453818b68a6c23c8b4120b227bc918c46cfcda0
Enes <60428493+enesbilik@users.noreply.github.com>
2020-12-06 15:49:56 +0300

    Create Number Triangle.c

---
+++ Number Triangle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/*
+n = 4 için şeklinde çıktı veren kod
+          1    
+       2     3    
+    4     5     6    
+ 7     8     9    10  
+
+*/
+
+int main() {
+	
+	int i,j,n;
+	int sayi = 1;
+	
+	printf("satir sayisi giriniz: ");
+	scanf("%d",&n);
+	int b = n-1;
+	
+	for (i=1; i<=n; i++) {
+		
+		for (j=0; j<b; j++) 
+			printf("   ");
+		
+		for (j=0; j<i; j++,sayi++)
+			printf("%2d    ",sayi);
+			
+		b--;
+		printf("\n");
+	}
+		
+	return 0;
+}
